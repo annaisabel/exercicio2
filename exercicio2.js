86e02072ae7c1fa515a9a86718d66d5ccd9a1e87
annaisabel <annaisabelloureiro@gmail.com>
2022-06-29 23:12:47 -0300

    first commit

---
+++ exercicio2.js
@@ -0,0 +1,4 @@
+const distanciaKm = 90
+const tempoHr = 3
+const medVelocidade = distanciaKm / tempoHr;
+console.log( `A velocidade média para chegar ao seu destino será de: ${medVelocidade} km/h`);
